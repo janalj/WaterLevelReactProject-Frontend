739b9944f8748fe37f67e880799c5cabeca5a150
janalj <44931863+janalj@users.noreply.github.com>
2022-05-26 23:38:18 +0000

    Added schoolchart for testing

--- src/App.jsx
+++ src/App.jsx
@@ -5,109 +5,101 @@ import Chart from 'chart.js/auto';
 
 function App() {
   const [seeMore, setSeeMore] = useState(false);
-  
   // when buttonAction is called, set seeMore variable to true
-  function buttonAction() {  
-      if (seeMore){
-        setSeeMore(false);
-      }else(setSeeMore(true))
+  function buttonAction() {
+    if (seeMore) {
+      setSeeMore(false);
+    } else (setSeeMore(true))
   }
-
-  if (seeMore) {
-
+  if (seeMore) {  // if seeMore is true 
     return (
-  
       <main>
-      
-      <button onClick={buttonAction}>See Less</button>
-      
-      <div id="datatext">
-        Here's a quick look at some of the data on reservoirs from the <a href="https://cdec.water.ca.gov/index.html">California Data Exchange Center</a>, which consolidates climate and water data from multiple federal and state government agencies, and  electric utilities.  Select a month and year to see storage levels in the eleven largest in-state reservoirs.
+        <button onClick={buttonAction}>See Less</button>
+        <div id="datatext">
+          Here's a quick look at some of the data on reservoirs from the <a href="https://cdec.water.ca.gov/index.html">California Data Exchange Center</a>, which consolidates climate and water data from multiple federal and state government agencies, and  electric utilities.  Select a month and year to see storage levels in the eleven largest in-state reservoirs.
       </div>
+ 
+      <SchoolChart schools={testProps}> </SchoolChart>
+        
+      </main>
       
-    </main>
       
-      
-      )
+    )
   }//end of if statement
-  else{
+  else { // if seeMore is false
     return (
       <main>
-         
         <button onClick={buttonAction}>See More</button>
-        
       </main>
     )
   }//end of else statement 
 
-  // Testing Data
-  let testProps = [{name: 'University of California-Davis', midIncome: 12875, sticker: 35532},{name: 'Stanford', midIncome: 3985, sticker: 74570},{name: 'California Polytechnic State University-San Luis Obispo', midIncome: 15601, sticker: 28927}]
+}//end of app
+
+// Testing Data
+let testProps = [{ name: 'University of California-Davis', midIncome: 12875, sticker: 35532 }, { name: 'Stanford', midIncome: 3985, sticker: 74570 }, { name: 'California Polytechnic State University-San Luis Obispo', midIncome: 15601, sticker: 28927 }]
 
 
-  
-  
-  function SchoolChart(props) {
+
+function SchoolChart(props) {
   const nicknames = new Map();
   nicknames.set(0, 'UC Davis');
   nicknames.set(1, 'Stanford');
   nicknames.set(2, 'Cal Poly-SLO');
-  
+
   if (props.schools) {
     let n = props.schools.length;
     console.log(props.schools);
 
     // objects containing row values
-    let stickerObj = {label: "Sticker Price",data: [], backgroundColor: ["pink"]}
-    let midIncObj = {label: "Middle Income Family Price", data: [], backgroundColor: ["red"]}
+    let stickerObj = { label: "Sticker Price", data: [], backgroundColor: ["pink"] }
+    let midIncObj = { label: "Middle Income Family Price", data: [], backgroundColor: ["red"] }
     let labels = [];
-    for (let i=0; i<n; i++) {
+    for (let i = 0; i < n; i++) {
       stickerObj.data.push(props.schools[i].sticker);
       midIncObj.data.push(props.schools[i].midIncome);
       labels.push(nicknames.get(i));
     }
 
 
-  let userData = {};
-  userData.labels = labels;
-  userData.datasets = [stickerObj, midIncObj];
+    let userData = {};
+    userData.labels = labels;
+    userData.datasets = [stickerObj, midIncObj];
 
-console.log(userData);
-let options = {
-  plugins: {
-    title: {
-      display: true,
-      text: 'Sticker vs. Middle Income Family Prices',
-    },
-  },
-  responsive: true,
-  maintainAspectRatio: false,
-  scales: {
-    x: {
-      grid: {
-        display: false
+    console.log(userData);
+    let options = {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Sticker vs. Middle Income Family Prices',
+        },
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          grid: {
+            display: false
+          }
+        },
+        y: {
+          grid: {
+            display: false
+          }
+        }
       }
-    },
-    y: {
-      grid: {
-        display: false
-      }
-    }
-  }
-};
+    };
 
 
-      return (
-        <div id="chart-container">
-          <Bar options={options} data={userData} />
-        </div>
-      )
+    return (
+      <div id="chart-container">
+        <Bar options={options} data={userData} />
+      </div>
+    )
   }
 }
 
 
 
-
-  
-}//end of app
 
 export default App;
