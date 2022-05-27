8358a9b40a2c7a0eff99b9295a2047e93b1d1d5f
janalj <44931863+janalj@users.noreply.github.com>
2022-05-27 01:58:34 +0000

    Updated chart

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,12 @@ function App() {
         <div id="datatext">
           Here's a quick look at some of the data on reservoirs from the <a href="https://cdec.water.ca.gov/index.html">California Data Exchange Center</a>, which consolidates climate and water data from multiple federal and state government agencies, and  electric utilities.  Select a month and year to see storage levels in the eleven largest in-state reservoirs.
       </div>
- 
-      <SchoolChart schools={testProps}> </SchoolChart>
-        
+
+        <SchoolChart schools={testProps}> </SchoolChart>
+
       </main>
-      
-      
+
+
     )
   }//end of if statement
   else { // if seeMore is false
@@ -37,56 +37,74 @@ function App() {
 }//end of app
 
 // Testing Data
-let testProps = [{ name: 'University of California-Davis', midIncome: 12875, sticker: 35532 }, { name: 'Stanford', midIncome: 3985, sticker: 74570 }, { name: 'California Polytechnic State University-San Luis Obispo', midIncome: 15601, sticker: 28927 }]
+let testProps = [{ name: 'University of California-Davis', midIncome: 12875, sticker: 30 }, { name: 'Stanford', midIncome: 3985, sticker: 20 }, { name: 'California Polytechnic State University-San Luis Obispo', midIncome: 15601, sticker: 40 }, { name: 'Stanford', midIncome: 3985, sticker: 50 }, { name: 'California Polytechnic State University-San Luis Obispo', midIncome: 15601, sticker: 35 }, { name: 'Stanford', sticker: 40 }, { name: 'California Polytechnic State University-San Luis Obispo', midIncome: 15601, sticker: 40 }, { name: 'Stanford', midIncome: 3985, sticker: 74570 }, { name: 'California Polytechnic State University-San Luis Obispo', midIncome: 15601, sticker: 25 }]
 
 
 
 function SchoolChart(props) {
   const nicknames = new Map();
-  nicknames.set(0, 'UC Davis');
-  nicknames.set(1, 'Stanford');
-  nicknames.set(2, 'Cal Poly-SLO');
+  nicknames.set(0, 'Shasta');
+  nicknames.set(1, 'Ororville');
+  nicknames.set(2, 'Trinity Lake');
+  nicknames.set(3, 'New Melones');
+  nicknames.set(4, 'San Luis');
+  nicknames.set(5, 'Don Pedro');
+  nicknames.set(6, 'Berryessa');
 
   if (props.schools) {
     let n = props.schools.length;
     console.log(props.schools);
 
     // objects containing row values
-    let stickerObj = { label: "Sticker Price", data: [], backgroundColor: ["pink"] }
-    let midIncObj = { label: "Middle Income Family Price", data: [], backgroundColor: ["red"] }
+    let stickerObj = { data: [], backgroundColor: ["rgb(120,199,227)"], barThickness: 40 }
     let labels = [];
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < 7; i++) {
       stickerObj.data.push(props.schools[i].sticker);
-      midIncObj.data.push(props.schools[i].midIncome);
       labels.push(nicknames.get(i));
     }
 
 
     let userData = {};
     userData.labels = labels;
-    userData.datasets = [stickerObj, midIncObj];
-
-    console.log(userData);
+    userData.datasets = [stickerObj];
+    
+    console.log("User Data",userData);
     let options = {
       plugins: {
-        title: {
-          display: true,
-          text: 'Sticker vs. Middle Income Family Prices',
+        // remove the label
+        legend: {
+          display: false
         },
       },
       responsive: true,
       maintainAspectRatio: false,
       scales: {
+        
         x: {
+          
           grid: {
             display: false
-          }
+          },
+          ticks: {
+                    maxRotation: 50,
+                    minRotation: 50,
+            
+                },
+          
         },
+        
         y: {
           grid: {
             display: false
-          }
+          }, 
+          min:10,
+          max: 80,
+          ticks: {
+          stepSize: 10
+        }      
+                    
         }
+       
       }
     };
 
