213c60b6bdcef7a1bbeea24f537a36d412884729
EmilyYang16 <>
2022-06-01 05:34:37 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
-import useAsyncFetch, { sendPostRequest } from './useAsyncFetch';
+import {useAsyncFetch, sendPostRequest} from './useAsyncFetch';
 import { Bar } from "react-chartjs-2";
 import Chart from 'chart.js/auto';
 import MonthPicker from './MonthPicker';
